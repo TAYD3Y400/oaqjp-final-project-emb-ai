929da4014e980b17d1947585eb7188be7f9dce1e
TAYD3Y400 <taylor20sep.hc@gmail.com>
2025-03-07 11:07:48 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
 
@@ -8,5 +9,21 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
 
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    higher = -1
+    higher_label = ""
+
+    for emotion in emotions.keys():
+        if emotions[emotion] > higher:
+            higher = emotions[emotion]
+            higher_label = emotion
+            
+    emotions["dominant_emotion"] = higher_label
+
+    return emotions
+
+# from emotion_detection import emotion_detector
+# emotion_detector("I love this new technology.")
